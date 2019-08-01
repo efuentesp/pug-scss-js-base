3575024602daa9fb0d20dd7c1a9d84ee603e407f
Edgar Felipe Fuentes Perea (EFP) <efuentesp@gmail.com>
2019-07-31 21:00:10 -0500

    JQuery UI funcionando con Accordion y Tabs.

--- dist/scripts/accimarket/01-caucion-bursatil.js
+++ dist/scripts/accimarket/01-caucion-bursatil.js
@@ -1,48 +1,52 @@
-console.log('01-caucion-bursatil');
+console.log("01-caucion-bursatil");
 
 $(function() {
-	$('#table_contratos').jqGrid({
-		url: 'http://localhost:3000/fideicomiso',
-		datatype: 'json',
-		mtype: 'GET',
-		colNames: [
-			'Contrato',
-			'Libro',
-			'Cliente',
-			'Digito',
-			'TV',
-			'Descr TV',
-			'Emisora',
-			'Serie',
-			'Cantidad',
-			'Precio',
-			'Valuación',
-			'Promotor',
-			'Folio'
-		],
-		colModel: [
-			{ name: 'generalesnumero', width: 55 },
-			{ name: 'generalesnombre', width: 90 },
-			{ name: 'generalesadministrador', width: 80, align: 'right' },
-			{ name: 'generalespromotor', width: 80, align: 'right' },
-			{ name: 'caracteristicasformamanejo', width: 80, align: 'right' },
-			{ name: 'caracteristicastiponegocio', width: 150, sortable: false },
-			{ name: 'caracteristicasproducto', width: 90 },
-			{ name: 'caracteristicasmontoapertura', width: 90 },
-			{ name: 'adicionalesnoescritura', width: 90 },
-			{ name: 'caracteristicasfechaalta', width: 90 },
-			{ name: 'adicionalesfechainscripcion', width: 90 },
-			{ name: 'adicionalesnombrenotario', width: 90 },
-			{ name: 'adicionalesnonotario', width: 90 }
-		],
-		pager: '#pager_contratos',
-		rowNum: 10,
-		rowList: [ 10, 20, 30 ],
-		sortname: 'generalesnumero',
-		sortorder: 'desc',
-		viewrecords: true,
-		gridview: true,
-		autoencode: true,
-		caption: ''
-	});
+  $("#criterios_busqueda_accordion").accordion({
+    collapsible: true
+  });
+
+  $("#table_contratos").jqGrid({
+    url: "http://localhost:3000/fideicomiso",
+    datatype: "json",
+    mtype: "GET",
+    colNames: [
+      "Contrato",
+      "Libro",
+      "Cliente",
+      "Digito",
+      "TV",
+      "Descr TV",
+      "Emisora",
+      "Serie",
+      "Cantidad",
+      "Precio",
+      "Valuación",
+      "Promotor",
+      "Folio"
+    ],
+    colModel: [
+      { name: "generalesnumero", width: 55 },
+      { name: "generalesnombre", width: 90 },
+      { name: "generalesadministrador", width: 80, align: "right" },
+      { name: "generalespromotor", width: 80, align: "right" },
+      { name: "caracteristicasformamanejo", width: 80, align: "right" },
+      { name: "caracteristicastiponegocio", width: 150, sortable: false },
+      { name: "caracteristicasproducto", width: 90 },
+      { name: "caracteristicasmontoapertura", width: 90 },
+      { name: "adicionalesnoescritura", width: 90 },
+      { name: "caracteristicasfechaalta", width: 90 },
+      { name: "adicionalesfechainscripcion", width: 90 },
+      { name: "adicionalesnombrenotario", width: 90 },
+      { name: "adicionalesnonotario", width: 90 }
+    ],
+    pager: "#pager_contratos",
+    rowNum: 10,
+    rowList: [10, 20, 30],
+    sortname: "generalesnumero",
+    sortorder: "desc",
+    viewrecords: true,
+    gridview: true,
+    autoencode: true,
+    caption: ""
+  });
 });
--- dist/scripts/accimarket/02-movimientos-por-contrato.js
+++ dist/scripts/accimarket/02-movimientos-por-contrato.js
@@ -1,48 +1,54 @@
-console.log('01-02-movimientos-por-contrato');
+console.log("01-02-movimientos-por-contrato");
 
 $(function() {
-	$('#table_movimientos_contrato').jqGrid({
-		url: 'http://localhost:3000/fideicomiso',
-		datatype: 'json',
-		mtype: 'GET',
-		colNames: [
-			'Contrato',
-			'Libro',
-			'Cliente',
-			'Digito',
-			'TV',
-			'Descr TV',
-			'Emisora',
-			'Serie',
-			'Cantidad',
-			'Precio',
-			'Valuación',
-			'Promotor',
-			'Folio'
-		],
-		colModel: [
-			{ name: 'generalesnumero', width: 55 },
-			{ name: 'generalesnombre', width: 90 },
-			{ name: 'generalesadministrador', width: 80, align: 'right' },
-			{ name: 'generalespromotor', width: 80, align: 'right' },
-			{ name: 'caracteristicasformamanejo', width: 80, align: 'right' },
-			{ name: 'caracteristicastiponegocio', width: 150, sortable: false },
-			{ name: 'caracteristicasproducto', width: 90 },
-			{ name: 'caracteristicasmontoapertura', width: 90 },
-			{ name: 'adicionalesnoescritura', width: 90 },
-			{ name: 'caracteristicasfechaalta', width: 90 },
-			{ name: 'adicionalesfechainscripcion', width: 90 },
-			{ name: 'adicionalesnombrenotario', width: 90 },
-			{ name: 'adicionalesnonotario', width: 90 }
-		],
-		pager: '#pager_movimientos_contrato',
-		rowNum: 10,
-		rowList: [ 10, 20, 30 ],
-		sortname: 'generalesnumero',
-		sortorder: 'desc',
-		viewrecords: true,
-		gridview: true,
-		autoencode: true,
-		caption: ''
-	});
+  $("#criterios_busqueda_accordion").accordion({
+    collapsible: true
+  });
+
+  $("#movimientos_por_contrato_tab_group").tabs();
+
+  $("#table_movimientos_contrato").jqGrid({
+    url: "http://localhost:3000/fideicomiso",
+    datatype: "json",
+    mtype: "GET",
+    colNames: [
+      "Contrato",
+      "Libro",
+      "Cliente",
+      "Digito",
+      "TV",
+      "Descr TV",
+      "Emisora",
+      "Serie",
+      "Cantidad",
+      "Precio",
+      "Valuación",
+      "Promotor",
+      "Folio"
+    ],
+    colModel: [
+      { name: "generalesnumero", width: 55 },
+      { name: "generalesnombre", width: 90 },
+      { name: "generalesadministrador", width: 80, align: "right" },
+      { name: "generalespromotor", width: 80, align: "right" },
+      { name: "caracteristicasformamanejo", width: 80, align: "right" },
+      { name: "caracteristicastiponegocio", width: 150, sortable: false },
+      { name: "caracteristicasproducto", width: 90 },
+      { name: "caracteristicasmontoapertura", width: 90 },
+      { name: "adicionalesnoescritura", width: 90 },
+      { name: "caracteristicasfechaalta", width: 90 },
+      { name: "adicionalesfechainscripcion", width: 90 },
+      { name: "adicionalesnombrenotario", width: 90 },
+      { name: "adicionalesnonotario", width: 90 }
+    ],
+    pager: "#pager_movimientos_contrato",
+    rowNum: 10,
+    rowList: [10, 20, 30],
+    sortname: "generalesnumero",
+    sortorder: "desc",
+    viewrecords: true,
+    gridview: true,
+    autoencode: true,
+    caption: ""
+  });
 });
